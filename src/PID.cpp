8e32124203ccbf2216dcde539b22cb16a9765270
chuongdao <cd.kernel.dev.105@gmail.com>
2018-11-07 23:38:32 -0800

    first working solution

--- src/PID.cpp
+++ src/PID.cpp
@@ -11,11 +11,20 @@ PID::PID() {}
 PID::~PID() {}
 
 void PID::Init(double Kp, double Ki, double Kd) {
+    this->Kp = Kp;
+    this->Kd = Kd;
+    this->Ki = Ki;
+    prev_d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+   p_error = cte;
+   d_error = cte - prev_d_error;
+   i_error += cte;
+   prev_d_error = cte;
 }
 
 double PID::TotalError() {
+    return p_error + i_error + d_error;
 }
 
--- src/PID.h
+++ src/PID.h
@@ -9,7 +9,7 @@ public:
   double p_error;
   double i_error;
   double d_error;
-
+  double prev_d_error;
   /*
   * Coefficients
   */ 
